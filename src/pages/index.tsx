0066b3721dc7bc08482478bb8d0a72303f3217b6
0xJayden <gannixcrypto@protonmail.com>
2024-04-01 20:18:21 -0700

    added game window

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,11 +33,22 @@ export default function Home() {
   const [audio, setAudio] = useState<HTMLAudioElement>();
   const [playing, setPlaying] = useState(false);
   const [openMap, setOpenMap] = useState(false);
-  const [focusedWindow, setFocusedWindow] = useState<"mint" | "map">("mint");
+  const [focusedWindow, setFocusedWindow] = useState<"mint" | "map" | "game">(
+    "mint"
+  );
+  const [openGame, setOpenGame] = useState(false);
 
   const requestRef = useRef<number>(0);
+  const timeRef = useRef<number>(0);
 
-  const animate = () => {
+  const animate = (t: number) => {
+    if (timeRef.current === 0) timeRef.current = t;
+    const timeElapsed = t - timeRef.current;
+    if (timeElapsed > 10000) {
+      timeRef.current = 0;
+      setDots([]);
+    }
+
     setDots((prev) => {
       return prev.map((dot) => {
         if (!dot.xAcceleration) return dot;
@@ -157,6 +168,8 @@ export default function Home() {
             setOpenMap={setOpenMap}
             openMap={openMap}
             setFocusedWindow={setFocusedWindow}
+            setOpenGame={setOpenGame}
+            openGame={openGame}
           />
           {openMint && (
             <MintWindow
@@ -172,12 +185,21 @@ export default function Home() {
               focusedWindow={focusedWindow}
             />
           )}
+          {openGame && (
+            <GameWindow
+              setOpenGame={setOpenGame}
+              setFocusedWindow={setFocusedWindow}
+              focusedWindow={focusedWindow}
+            />
+          )}
           <BottomNavbar
             setOpenMint={setOpenMint}
             openMint={openMint}
             setOpenMap={setOpenMap}
             openMap={openMap}
             setFocusedWindow={setFocusedWindow}
+            setOpenGame={setOpenGame}
+            openGame={openGame}
           />
         </>
       )}
@@ -214,8 +236,8 @@ const MintWindow = ({
   focusedWindow,
 }: {
   setOpenMint: Dispatch<SetStateAction<boolean>>;
-  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map">>;
-  focusedWindow: "mint" | "map";
+  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map" | "game">>;
+  focusedWindow: "mint" | "map" | "game";
 }) => {
   const [dots, setDots] = useState<Array<Dot>>([]);
   const [price, setPrice] = useState<number>();
@@ -434,7 +456,10 @@ const MintWindow = ({
           className="absolute z-30 inset-0 cursor-default"
         ></button>
         <button
-          onClick={() => setOpenMint(false)}
+          onClick={() => {
+            setOpenMint(false);
+            return () => cancelAnimationFrame(requestRef.current);
+          }}
           className="bg-red-500 rounded-full h-4 aspect-square flex justify-center items-center z-40 absolute top-1 left-1"
         >
           <Icon path={mdiClose} className="h-3 text-black" />
@@ -522,12 +547,16 @@ const IconContainer = ({
   setOpenMap,
   openMap,
   setFocusedWindow,
+  setOpenGame,
+  openGame,
 }: {
   setOpenMint: Dispatch<SetStateAction<boolean>>;
   openMint: boolean;
   setOpenMap: Dispatch<SetStateAction<boolean>>;
   openMap: boolean;
-  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map">>;
+  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map" | "game">>;
+  setOpenGame: Dispatch<SetStateAction<boolean>>;
+  openGame: boolean;
 }) => {
   return (
     <div className="absolute space-y-4 p-4 inset-0 z-10">
@@ -541,6 +570,11 @@ const IconContainer = ({
         openMap={openMap}
         setFocusedWindow={setFocusedWindow}
       />
+      <GameIcon
+        setOpenGame={setOpenGame}
+        openGame={openGame}
+        setFocusedWindow={setFocusedWindow}
+      />
     </div>
   );
 };
@@ -552,7 +586,7 @@ const MintIcon = ({
 }: {
   setOpenMint: Dispatch<SetStateAction<boolean>>;
   openMint: boolean;
-  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map">>;
+  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map" | "game" | "game">>;
 }) => {
   const openWindow = () => {
     setOpenMint(true);
@@ -583,7 +617,7 @@ const MapIcon = ({
 }: {
   setOpenMap: Dispatch<SetStateAction<boolean>>;
   openMap: boolean;
-  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map">>;
+  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map" | "game">>;
 }) => {
   const openWindow = () => {
     setOpenMap(true);
@@ -607,23 +641,61 @@ const MapIcon = ({
   );
 };
 
+const GameIcon = ({
+  setOpenGame,
+  openGame,
+  setFocusedWindow,
+}: {
+  setOpenGame: Dispatch<SetStateAction<boolean>>;
+  openGame: boolean;
+  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map" | "game" | "game">>;
+}) => {
+  const openWindow = () => {
+    setOpenGame(true);
+    setFocusedWindow("game");
+  };
+
+  return (
+    <button
+      onClick={openWindow}
+      className="h-20 w-20 flex items-center justify-center flex-col"
+    >
+      <Image
+        src={"/gameicon.png"}
+        width={100}
+        height={100}
+        alt="gameicon"
+        className="w-10 h-10"
+      />
+      <p className="text-sm drop-shadow text-center">Play Game</p>
+    </button>
+  );
+};
+
 const BottomNavbar = ({
   setOpenMint,
   openMint,
   setOpenMap,
   openMap,
   setFocusedWindow,
+  setOpenGame,
+  openGame,
 }: {
   setOpenMint: Dispatch<SetStateAction<boolean>>;
   openMint: boolean;
   setOpenMap: Dispatch<SetStateAction<boolean>>;
   openMap: boolean;
-  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map">>;
+  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map" | "game">>;
+  setOpenGame: Dispatch<SetStateAction<boolean>>;
+  openGame: boolean;
 }) => {
-  const openWindow = (window: "mint" | "map") => {
+  const openWindow = (window: "mint" | "map" | "game") => {
     if (window === "mint") {
       setOpenMint(!openMint);
       setFocusedWindow("mint");
+    } else if (window === "game") {
+      setOpenGame(!openGame);
+      setFocusedWindow("game");
     } else {
       setOpenMap(!openMap);
       setFocusedWindow("map");
@@ -650,7 +722,7 @@ const BottomNavbar = ({
         </button>
         <button
           onClick={() => openWindow("map")}
-          className="flex flex-col items-center space-y-1 rounded-full"
+          className="flex flex-col pb-1 items-center space-y-1 rounded-full"
         >
           <Image
             className="h-10 w-10"
@@ -660,6 +732,19 @@ const BottomNavbar = ({
             height={100}
           />
           {openMap && <div className="rounded-full bg-white h-1 w-1"></div>}
+        </button>
+        <button
+          onClick={() => openWindow("game")}
+          className="flex flex-col items-center space-y-1"
+        >
+          <Image
+            className="h-10 w-10"
+            src={"/gameicon.png"}
+            alt="gameicon"
+            width={100}
+            height={100}
+          />
+          {openGame && <div className="rounded-full bg-white h-1 w-1"></div>}
         </button>
       </div>
     </div>
@@ -910,8 +995,8 @@ const MapWindow = ({
   focusedWindow,
 }: {
   setOpenMap: Dispatch<SetStateAction<boolean>>;
-  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map">>;
-  focusedWindow: "mint" | "map";
+  setFocusedWindow: Dispatch<SetStateAction<"mint" | "map" | "game">>;
+  focusedWindow: "mint" | "map" | "game";
 }) => {
   return (
     <div
@@ -945,6 +1030,272 @@ const MapWindow = ({
             playsInline
           />
         </div>
+      </div>
+    </div>
+  );
+};
+
+type GameObject = {
+  x: number;
+  y: number;
+  xSpeed: number;
+  ySpeed: number;
+  yAcceleration: number;
+  xAcceleration: number;
+  size: number;
+};
+
+type PlayerObject = {
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+};
+
+const GameWindow = ({
+  setOpenGame,
+  setFocusedWindow,
+  focusedWindow,
+}: {
+  setOpenGame: Dispatch<SetStateAction<boolean>>;
+  setFocusedWindow: Dispatch<SetStateAction<"game" | "map" | "mint">>;
+  focusedWindow: "game" | "map" | "mint";
+}) => {
+  const [gameObject, setGameObject] = useState<GameObject>();
+  const [playerObject, setPlayerObject] = useState<PlayerObject>();
+  const [gameStatus, setGameStatus] = useState<"playing" | "paused" | "over">(
+    "paused"
+  );
+
+  const requestRef = useRef<number>(0);
+  const gameWindowRef = useRef<HTMLDivElement>(null);
+  const playerRef = useRef<PlayerObject>();
+  const score = useRef<number>(0);
+  const collisionDetected = useRef<boolean>(false);
+
+  const animate = () => {
+    setGameObject((prev) => {
+      if (!prev) return prev;
+      if (!gameWindowRef.current) return;
+
+      let newX = prev.x + prev.xSpeed;
+      let newY = prev.y + prev.ySpeed;
+
+      if (!playerRef.current) {
+        console.log("No player object");
+        return prev;
+      }
+
+      if (newX >= gameWindowRef.current.offsetWidth - 10 || newX <= 0) {
+        prev.xSpeed = -prev.xSpeed;
+      }
+
+      if (newY >= gameWindowRef.current.offsetHeight - 20) {
+        setGameStatus("over");
+        return prev;
+      }
+
+      if (gameStatus === "over") return;
+
+      if (
+        !collisionDetected.current &&
+        newX + prev.size >= playerRef.current.x &&
+        newX <= playerRef.current.x + playerRef.current.width &&
+        newY + prev.size >= playerRef.current.y &&
+        newY <= playerRef.current.y + playerRef.current.height
+      ) {
+        collisionDetected.current = true;
+        score.current += 1;
+      } else if (
+        newX + prev.size < playerRef.current.x ||
+        newX > playerRef.current.x + playerRef.current.width ||
+        newY + prev.size < playerRef.current.y ||
+        newY > playerRef.current.y + playerRef.current.height
+      ) {
+        collisionDetected.current = false;
+      }
+
+      if (collisionDetected.current) {
+        return {
+          x: Math.random() * gameWindowRef.current.offsetWidth,
+          y: 10,
+          xSpeed:
+            prev.xSpeed < 0
+              ? prev.xSpeed - prev.xAcceleration
+              : prev.xSpeed +
+                prev.xAcceleration * (Math.random() * 2 > 1 ? 1 : -1),
+          ySpeed: prev.ySpeed + prev.yAcceleration,
+          yAcceleration: prev.yAcceleration + 0.01,
+          xAcceleration: prev.xAcceleration + 0.01,
+          size: prev.size,
+        };
+      } else {
+        return {
+          x: newX,
+          y: newY,
+          xSpeed: prev.xSpeed,
+          ySpeed: prev.ySpeed,
+          yAcceleration: prev.yAcceleration,
+          xAcceleration: prev.xAcceleration,
+          size: prev.size,
+        };
+      }
+    });
+
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) => {
+      setPlayerObject((prev) => {
+        if (!prev) return prev;
+        let newX = e.clientX - 150;
+
+        if (!gameWindowRef.current) return prev;
+
+        if (newX > gameWindowRef.current?.offsetWidth - 85)
+          newX = gameWindowRef.current?.offsetWidth - 85;
+
+        if (newX < 0) newX = 0;
+
+        return {
+          x: newX,
+          y: prev.y,
+          height: prev.height,
+          width: prev.width,
+        };
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!gameWindowRef.current) return;
+
+    if (gameStatus !== "playing") return;
+
+    setPlayerObject({
+      x: 0,
+      y: gameWindowRef.current.offsetHeight - 80,
+      height: 90,
+      width: 80,
+    });
+
+    setGameObject({
+      x: gameWindowRef.current.offsetWidth / 2,
+      y: 10,
+      xSpeed: 2 * (Math.random() * 2 > 1 ? 1 : -1),
+      ySpeed: 2,
+      yAcceleration: 1.05,
+      xAcceleration: 1.05,
+      size: 20,
+    });
+
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [gameStatus]);
+
+  useEffect(() => {
+    playerRef.current = playerObject;
+  }, [playerObject]);
+
+  return (
+    <div
+      className={`absolute h-[60%] md:h-[70%] animate-open w-[75%] -top-20 left-5 bottom-16 my-auto`}
+      style={{ zIndex: focusedWindow === "game" ? 30 : 20 }}
+    >
+      <div
+        ref={gameWindowRef}
+        className="flex flex-col h-full justify-center items-center relative border-2 rounded bg-black"
+      >
+        <button
+          onClick={() => setFocusedWindow("map")}
+          className="absolute cursor-default z-30 inset-0"
+        ></button>
+        <button
+          onClick={() => {
+            setOpenGame(false);
+            return () => {
+              document.removeEventListener("mousemove", (e) => {
+                setPlayerObject((prev) => {
+                  if (!prev) return prev;
+                  let newX = e.clientX - 150;
+
+                  if (!gameWindowRef.current) return prev;
+
+                  if (newX > gameWindowRef.current?.offsetWidth - 85)
+                    newX = gameWindowRef.current?.offsetWidth - 85;
+
+                  if (newX < 0) newX = 0;
+
+                  return {
+                    x: newX,
+                    y: prev.y,
+                    height: prev.height,
+                    width: prev.width,
+                  };
+                });
+              });
+            };
+          }}
+          className="bg-red-500 rounded-full h-4 aspect-square flex justify-center items-center z-40 absolute top-1 left-1"
+        >
+          <Icon path={mdiClose} className="h-3 text-black" />
+        </button>
+        <div className="items-center absolute z-20 w-full space-x-2 flex top-0 left-0 h-6 p-1 right-0 bg-gradient-to-r from-[#c9daff] via-[#eeeeee] to-[#c9daff]">
+          <p className="text-white pl-6 text-sm drop-shadow font-bold">
+            Paingelz Game
+          </p>
+        </div>
+        <div className="absolute top-10 right-10">
+          <p>Score: {score.current}</p>
+        </div>
+        {gameStatus === "paused" && (
+          <button
+            onClick={() => setGameStatus("playing")}
+            className="absolute z-30 inset-0 bg-black bg-opacity-70 flex justify-center items-center"
+          >
+            <p className="text-white text-2xl">Play</p>
+          </button>
+        )}
+        {gameStatus === "over" && (
+          <div className="absolute z-30 bg-black bg-opacity-70 inset-0 flex flex-col justify-center items-center">
+            <p className="text-white pb-4 text-2xl">Game Over</p>
+            <p className="text-white pb-4 text-2xl">Score: {score.current}</p>
+            <button
+              className="p-2 px-4 border"
+              onClick={() => {
+                score.current = 0;
+                setGameStatus("playing");
+              }}
+            >
+              Play Again
+            </button>
+          </div>
+        )}
+        {gameObject && (
+          <img
+            src="/star.png"
+            className="absolute"
+            style={{
+              top: gameObject.y + "px",
+              left: gameObject.x + "px",
+              height: gameObject.size + "px",
+              width: gameObject.size + "px",
+            }}
+          ></img>
+        )}
+        {playerObject && (
+          <img
+            className="absolute"
+            src="/player.png"
+            style={{
+              top: playerObject.y + "px",
+              left: playerObject.x + "px",
+              height: playerObject.height + "px",
+              width: playerObject.width + "px",
+            }}
+          ></img>
+        )}
       </div>
     </div>
   );
